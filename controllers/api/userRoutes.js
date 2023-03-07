dab0c966a13ee7a9bd6444218889b940bcb990a5
Harrison Boatman <hrry.boatman2@gmail.com>
2023-03-06 18:39:01 -0600

    fixed users route for heroku

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -17,14 +17,6 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.get('/', async (req,res) => {
-    try {
-        userData = await User.findAll();
-        res.status(200).json(userData)
-    } catch (err) {
-        res.status(500).json(err)
-    }
-})
 
 router.post('/login', async (req, res) => {
     // try {
